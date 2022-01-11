a5d432ef74c6ddcc74c2b4d0266030657b3908a5
ASAHISURFACE\asmai <a.s.mail071275@gmail.com>
2022-01-11 09:47:58 +0900

    220111

---
+++ Solidity/createNFT.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+import "https://github.com/nibbstack/erc721/blob/master/src/contracts/tokens/nf-token-metadata.sol";
+import "https://github.com/nibbstack/erc721/blob/master/src/contracts/ownership/ownable.sol";
+
+contract NewNFT is NFTokenMetadata, Ownable {
+
+    constructor(){
+        nftName = "Synth NFT";
+        nftSymbol = "SYN";
+     }
+    
+    //_uriにIPFSのアドレスが入る
+    function mint(address _to ,uint256 _tokenId, string calldata _uri) external onlyOwner{
+        super._mint(_to, _tokenId);
+        super._setTokenUri(_tokenId, _uri);
+    }
+}
